387eadc038fe42e41a83be92eda9d5de47d643e4
Dmitry Mishenko <chillfont@gmail.com>
2025-04-21 11:50:09 +0700

    Параметризация Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -2,16 +2,28 @@ from playwright.sync_api import expect, Page
 import pytest
 
 
+@pytest.mark.parametrize('email, password',
+                         [
+                             ('user.name@gmail.com', 'password'),
+                             ('user.name@gmail.com', '  '),
+                             ('  ', 'password')
+                         ],
+                         ids=[
+                             'Invalid email and invalid password',
+                             'Invalid email and empty password',
+                             'Empty email and invalid password'
+                         ]
+                         )
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization(chromium_page: Page):
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
     email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
+    email_input.fill(f'{email}')
 
     password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill('password')
+    password_input.fill(f'{password}')
 
     login_button = chromium_page.get_by_test_id('login-page-login-button')
     login_button.click()
