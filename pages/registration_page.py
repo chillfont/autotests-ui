d759909516b0c8948474ad142952cef3bcd19643
Dmitry Mishenko <chillfont@gmail.com>
2025-04-23 10:14:25 +0700

    Знакомство с PageFactory

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import Page, expect
 
 from components.authentication.registration_form_component import RegistrationFormComponent
+from elements.button import Button
+from elements.link import Link
 from pages.base_page import BasePage
 
 
@@ -10,8 +12,10 @@ class RegistrationPage(BasePage):
 
         self.registration_form = RegistrationFormComponent(page)
 
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
-        self.login_link = page.get_by_test_id('registration-page-login-link')
+        self.registration_button = Button(
+            page, 'registration-page-registration-button', 'Registration button'
+        )
+        self.login_link = Link(page, 'registration-page-login-link', 'Login link')
 
     def check_registration_form_visible(self, email: str, username: str, password: str):
         self.registration_form.check_visible(email, username, password)
