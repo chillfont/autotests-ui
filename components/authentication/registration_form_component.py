3435b53f3b2d33871106b351434e5aedf8b75ba0
Dmitry Mishenko <chillfont@gmail.com>
2025-04-28 08:45:29 +0700

    Добавление Allure шагов в автотесты

--- components/authentication/registration_form_component.py
+++ components/authentication/registration_form_component.py
@@ -1,3 +1,4 @@
+import allure
 from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
@@ -12,6 +13,7 @@ class RegistrationFormComponent(BaseComponent):
         self.username_input = Input(page, 'registration-form-username-input', 'Username input')
         self.password_input = Input(page, 'registration-form-password-input', 'Password input')
 
+    @allure.step("Fill registration form")
     def fill(self, email, username, password):
         self.email_input.fill(email)
         self.email_input.check_have_value(email)
@@ -22,6 +24,7 @@ class RegistrationFormComponent(BaseComponent):
         self.password_input.fill(password)
         self.password_input.check_have_value(password)
 
+    @allure.step("Check visible registration form")
     def check_visible(self, email, username, password):
         self.email_input.check_visible()
         self.email_input.check_have_value(email)
