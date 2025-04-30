08a2ea52a7211d2687fbb9f386c7fecda00bc9cf
Dmitry Mishenko <chillfont@gmail.com>
2025-04-30 09:06:10 +0700

    Практикуемся в интеграции покрытия в UI тесты

--- elements/button.py
+++ elements/button.py
@@ -1,5 +1,6 @@
 import allure
 from playwright.sync_api import expect
+from ui_coverage_tool import ActionType
 
 from elements.base_element import BaseElement
 from tools.logger import get_logger
@@ -20,10 +21,14 @@ class Button(BaseElement):
             logger.info(step)
             expect(locator).to_be_enabled()
 
+        self.track_coverage(ActionType.ENABLED, nth, **kwargs)
+
     def check_disabled(self, nth: int = 0, **kwargs):
-        step = f'Checking that {self.type_of} "self.name" is disbaled'
+        step = f'Checking that {self.type_of} "self.name" is disabled'
 
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_disabled()
+
+        self.track_coverage(ActionType.DISABLED, nth, **kwargs)
